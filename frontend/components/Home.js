7d29c0397ce1c3b4db245c770b4a3b386b7a98a7
carde'a weaver <165233514+Cardeaweaver@users.noreply.github.com>
2024-09-24 12:28:45 -0400

    Update Home.js

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -1,4 +1,3 @@
-'
 import React from 'react';
 import pizza from './images/pizza.jpg';
 import { useNavigate } from 'react-router-dom';
@@ -6,15 +5,15 @@ import { useNavigate } from 'react-router-dom';
 function Home() {
   const navigate = useNavigate();
 
-  const   
- handlePizzaClick = () => {
+  const handlePizzaClick = () => {
     navigate('/order');
   };
 
   return (
     <div>
       <h2>Welcome to Bloom Pizza!</h2>
-      {/* clicking on the img should navigate to "/order" */}
+      
+{/* clicking on the img should navigate to "/order" */}
       <img
         alt="order-pizza"
         style={{ cursor: 'pointer' }}
@@ -25,16 +24,4 @@ function Home() {
   );
 }
 
-function Home() {
-  return (
-    <div>
-      <h2>
-        Welcome to Bloom Pizza!
-      </h2>
-      
-      <img alt="order-pizza" style={{ cursor: 'pointer' }} src={pizza} />
-    </div>
-  )
-}
-
-export default Home
+export default Home;
